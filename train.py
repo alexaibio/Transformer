f02acfcd25abb8cbafe8b541205ef6a77ebb3e97
Alex <alex@bioinfo>
2024-01-19 20:47:09 +0100

    Move data processing in a separate package

--- train.py
+++ train.py
@@ -1,19 +1,7 @@
-# Huggingface datasets and tokenizers
-from datasets import load_dataset
-from tokenizers import Tokenizer
-from tokenizers.models import WordLevel
-from tokenizers.trainers import WordLevelTrainer
-from tokenizers.pre_tokenizers import Whitespace
-
 from model.model import build_transformer
-from dataset import BilingualDataset, causal_mask
 from config import get_config, get_weights_file_path, latest_weights_file_path
-
-import torchtext.datasets as datasets
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader, random_split
-from torch.optim.lr_scheduler import LambdaLR
 import warnings
 from tqdm import tqdm
 import os
@@ -21,6 +9,9 @@ from pathlib import Path
 import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 from utils import vizualize_model_by_keys
+from data_process.build_dataset import get_ds
+from data_process.dataset import causal_mask
+
 
 # Define the device
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
@@ -35,82 +26,8 @@ elif (device == 'mps'):
 else:
     print("NOTE: If you have a GPU, consider using it for training.")
     print("      On a Windows machine with NVidia GPU, check this video: https://www.youtube.com/watch?v=GMSjDTU8Zlc")
-    print(
-        "      On a Mac machine, run: pip3 install --pre torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/nightly/cpu")
+    print("      On a Mac machine, run: pip3 install --pre torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/nightly/cpu")
 device = torch.device(device)
-
-
-
-def get_all_sentences(ds, lang):
-    for item in ds:
-        yield item['translation'][lang]
-
-
-def get_or_build_tokenizer(config, ds, lang):
-    tokenizer_path = Path(config['tokenizer_file'].format(lang))
-
-    if not Path.exists(tokenizer_path):
-        # Most code taken from: https://huggingface.co/docs/tokenizers/quicktour
-        # if tokenizer see a word not in vocabulary it substitutes it with UNK
-        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
-        tokenizer.pre_tokenizer = Whitespace()   # split by white space
-
-        # [SOS] - start of sentence / [EOS] - end of sentence
-        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
-        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
-        tokenizer.save(str(tokenizer_path))
-    else:
-        tokenizer = Tokenizer.from_file(str(tokenizer_path))
-    return tokenizer
-
-
-def get_ds(config):
-    # It only has the train split, so we divide it overselves - from huggingface
-    ds_raw = load_dataset(
-        f"{config['datasource']}",
-        f"{config['lang_src']}-{config['lang_tgt']}",
-        split='train'
-    )
-
-    # Build tokenizers
-    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
-    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
-
-    # Keep 90% for training, 10% for validation
-    train_ds_size = int(0.9 * len(ds_raw))
-    val_ds_size = len(ds_raw) - train_ds_size
-    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
-
-    # create tensors
-    train_ds = BilingualDataset(
-        train_ds_raw,
-        tokenizer_src, tokenizer_tgt,
-        config['lang_src'], config['lang_tgt'], config['seq_len']
-    )
-
-    val_ds = BilingualDataset(
-        val_ds_raw,
-        tokenizer_src, tokenizer_tgt,
-        config['lang_src'], config['lang_tgt'], config['seq_len']
-    )
-
-    # Find the maximum length of each sentence in the source and target sentence
-    max_len_src = 0
-    max_len_tgt = 0
-
-    for item in ds_raw:
-        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
-        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
-        max_len_src = max(max_len_src, len(src_ids))
-        max_len_tgt = max(max_len_tgt, len(tgt_ids))
-
-    print(f'Max length of source sentence: {max_len_src}')
-    print(f'Max length of target sentence: {max_len_tgt}')
-
-    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
-    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
-
-    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
 
 
 def get_model(config, vocab_src_len, vocab_tgt_len):
