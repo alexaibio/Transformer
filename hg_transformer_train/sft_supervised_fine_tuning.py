c57a2ce5db632418a1ff8c799018ef12113418fb
Alex <alex@bioinfo>
2024-10-10 17:21:03 +0200

    SFT debug

--- hg_transformer_train/sft_supervised_fine_tuning.py
+++ hg_transformer_train/sft_supervised_fine_tuning.py
@@ -7,6 +7,12 @@ from datasets import load_dataset
 from datasets import DatasetDict
 from transformers import AutoTokenizer
 from huggingface_hub import login
+import re
+import random
+import torch
+from multiprocessing import cpu_count
+from transformers import BitsAndBytesConfig
+
 
 # Enter your Hugging Face token when prompted
 login()
@@ -35,24 +41,19 @@ for message in messages:
 model_id = "mistralai/Mistral-7B-v0.1"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
+# LlamaTokenizerFast is a BPE-based tokenizer
+print(f"Tokenizer class: {tokenizer.__class__.__name__}")
 
-# set pad_token_id equal to the eos_token_id if not set
+######## set PADDING for whole context length
 if tokenizer.pad_token_id is None:
     tokenizer.pad_token_id = tokenizer.eos_token_id
 
-# Set reasonable default for models without max length
 if tokenizer.model_max_length > 100_000:
-    tokenizer.model_max_length = 2048
+    tokenizer.model_max_length = 2048       # set to maximum context length ( T )
 
-# Set chat template
+#### Set and apply Jinja2 chat template to convert dictionary to string
 DEFAULT_CHAT_TEMPLATE = "{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}"
 tokenizer.chat_template = DEFAULT_CHAT_TEMPLATE
-
-
-######## Apply chat template
-import re
-import random
-from multiprocessing import cpu_count
 
 def apply_chat_template(example, tokenizer):
     messages = example["messages"]
@@ -64,10 +65,12 @@ def apply_chat_template(example, tokenizer):
     return example
 
 column_names = list(raw_datasets["train"].features)
+
+# run on multiple cores
 raw_datasets = raw_datasets.map(apply_chat_template,
                                 num_proc=cpu_count(),
                                 fn_kwargs={"tokenizer": tokenizer},
-                                remove_columns=column_names,
+                                remove_columns=column_names,    # which columns to remove from the dataset after applying the function.
                                 desc="Applying chat template",)
 
 # create the splits
@@ -76,3 +79,97 @@ eval_dataset = raw_datasets["test"]
 
 for index in random.sample(range(len(raw_datasets["train"])), 3):
   print(f"Sample {index} of the processed training set:\n\n{raw_datasets['train'][index]['text']}")
+
+
+
+
+
+
+
+# quantize and LoRa
+quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype="torch.bfloat16",
+)
+device_map = {"": torch.cuda.current_device()} if torch.cuda.is_available() else None
+
+model_kwargs = dict(
+    attn_implementation="flash_attention_2", # set this to True if your GPU supports it (Flash Attention drastically speeds up model computations)
+    torch_dtype="auto",
+    use_cache=False, # set to False as we're going to use gradient checkpointing
+    device_map=device_map,
+    quantization_config=quantization_config,
+)
+
+
+## Define SFTTrainer from TRL library.
+# This class inherits from the Trainer class available in the Transformers library,
+# but is specifically optimized for supervised fine-tuning (instruction tuning).
+from trl import SFTTrainer
+from peft import LoraConfig
+from transformers import TrainingArguments
+
+# path where the Trainer will save its checkpoints and logs
+output_dir = 'data/check_points_Mistral-7B-v0.1'
+
+# based on config
+training_args = TrainingArguments(
+    fp16=True, # specify bf16=True instead when training on GPUs that support bf16
+    do_eval=True,
+    evaluation_strategy="epoch",
+    gradient_accumulation_steps=128,
+    gradient_checkpointing=True,
+    gradient_checkpointing_kwargs={"use_reentrant": False},
+    learning_rate=2.0e-05,
+    log_level="info",
+    logging_steps=5,
+    logging_strategy="steps",
+    lr_scheduler_type="cosine",
+    max_steps=-1,
+    num_train_epochs=1,
+    output_dir=output_dir,
+    overwrite_output_dir=True,
+    per_device_eval_batch_size=1, # originally set to 8
+    per_device_train_batch_size=1, # originally set to 8
+    # push_to_hub=True,
+    # hub_model_id="zephyr-7b-sft-lora",
+    # hub_strategy="every_save",
+    # report_to="tensorboard",
+    save_strategy="no",
+    save_total_limit=None,
+    seed=42,
+)
+
+# based on config
+peft_config = LoraConfig(
+        r=64,
+        lora_alpha=16,
+        lora_dropout=0.1,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+)
+
+trainer = SFTTrainer(
+        model=model_id,
+        model_init_kwargs=model_kwargs,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        dataset_text_field="text",
+        tokenizer=tokenizer,
+        packing=True,
+        peft_config=peft_config,
+        max_seq_length=tokenizer.model_max_length,
+    )
+
+train_result = trainer.train()
+
+metrics = train_result.metrics
+max_train_samples = training_args.max_train_samples if training_args.max_train_samples is not None else len(train_dataset)
+metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+trainer.log_metrics("train", metrics)
+trainer.save_metrics("train", metrics)
+trainer.save_state()
+
